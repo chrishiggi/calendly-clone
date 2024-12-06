7d41c0ce798c17da2ea48f80cecc668ee9e0689e
Chris Higgi <chrishiggi@gmail.com>
2024-12-06 15:30:23 +0000

    added simple homepages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({subsets: ["latin"], variable: "--font-sans"}); 
 
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className} >
+      <body className={cn("min-h-screen, bg-background, font-sans, antialiased", inter.variable)} >
         {children}
       </body>
     </html>
